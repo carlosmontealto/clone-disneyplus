41ebfc15a16dbef31b0e1615c7057e0ceec4cdec
Carlos Monte Alto <kadu1401@gmail.com>
2023-08-08 16:51:31 -0300

    adição do Header

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+            ocutalElementosHeader();
+        } else {
+            exibeElementosHeader();
+        }
+    })
+
+
+    //Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
+    //Seção FAQ, accordion
     for (let i = 0; i <questions.length; i++){
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
@@ -39,4 +55,14 @@ function escondeTodasAsAbas(){
     for (let i = 0; i< tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+function ocutalElementosHeader (){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
